b0e492d69cb6277e2bd25bdab8999d0c1a572007
mingj7235 <joshuara7235@gmail.com>
2024-12-01 18:44:54 +0900

    feat: JWT Provider 를 개발한다.

--- src/main/kotlin/com/mj/chat/common/exception/ErrorCode.kt
+++ src/main/kotlin/com/mj/chat/common/exception/ErrorCode.kt
@@ -8,6 +8,10 @@ enum class ErrorCode(
     USER_SAVED_FAILED(1, "user saved failed"),
     NOT_EXIST_USER(2, "not exist user"),
     NOT_MATCH_PASSWORD(3, "not match password"),
+
+    ACCESS_TOKEN_IS_NOT_EXPIRED(4, "access token is not expired"),
+    TOKEN_IS_INVALID(5, "access token is invalid"),
+    TOKEN_IS_EXPIRED(6, "access token is expired"),
     ;
 
     override fun getCode(): Int = code
--- src/main/kotlin/com/mj/chat/security/JwtProvider.kt
+++ src/main/kotlin/com/mj/chat/security/JwtProvider.kt
@@ -2,7 +2,15 @@ package com.mj.chat.security
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.exceptions.AlgorithmMismatchException
+import com.auth0.jwt.exceptions.InvalidClaimException
+import com.auth0.jwt.exceptions.SignatureVerificationException
+import com.auth0.jwt.exceptions.TokenExpiredException
+import com.auth0.jwt.interfaces.DecodedJWT
 import com.mj.chat.common.constants.Constants.Companion.ON_MINUTE_TO_MILLIS
+import com.mj.chat.common.exception.CustomException
+import com.mj.chat.common.exception.ErrorCode
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.util.Date
@@ -14,6 +22,8 @@ class JwtProvider(
     @Value("\${token.refresh-secret-key}") private val refreshSecretKey: String,
     @Value("\${token.refresh-token-time}") private val refreshTokenTime: Long,
 ) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     fun createToken(userName: String): String =
         JWT
             .create()
@@ -29,4 +39,46 @@ class JwtProvider(
             .withIssuedAt(Date())
             .withExpiresAt(Date(System.currentTimeMillis() + refreshTokenTime * ON_MINUTE_TO_MILLIS))
             .sign(Algorithm.HMAC256(refreshSecretKey))
+
+    // refresh token 을 발행하기 위한 decode. 기존 token 이 expired 되어야 한다.
+    fun checkTokenForRefresh(token: String): DecodedJWT {
+        try {
+            val decodedJWT = JWT.require(Algorithm.HMAC256(secretKey)).build().verify(token)
+            logger.error("token must be expired, ${decodedJWT.subject}")
+            throw CustomException(ErrorCode.ACCESS_TOKEN_IS_NOT_EXPIRED)
+        } catch (e: Exception) {
+            when (e) {
+                is AlgorithmMismatchException, is SignatureVerificationException, is InvalidClaimException,
+                -> throw CustomException(ErrorCode.TOKEN_IS_INVALID) // must be changed
+                else -> throw e
+            }
+        } catch (e: TokenExpiredException) {
+            // 기존 token 이 만료되었으므로, decode 를 수행한다.
+            return JWT.decode(token)
+        }
+    }
+
+    fun decodeAccessToken(token: String): DecodedJWT = decodeTokenAfterVerify(token, secretKey)
+
+    fun decodeRefreshToken(token: String): DecodedJWT = decodeTokenAfterVerify(token, refreshSecretKey)
+
+    // token 이 정상적으로 검증하고 decode 된 jwt 를 리턴하는 함수
+    private fun decodeTokenAfterVerify(
+        token: String,
+        key: String,
+    ): DecodedJWT {
+        try {
+            return JWT.require(Algorithm.HMAC256(key)).build().verify(token)
+        } catch (e: Exception) {
+            when (e) {
+                is AlgorithmMismatchException, is SignatureVerificationException, is InvalidClaimException,
+                -> throw CustomException(ErrorCode.TOKEN_IS_INVALID)
+                else -> throw e
+            }
+        } catch (e: TokenExpiredException) {
+            throw CustomException(ErrorCode.TOKEN_IS_EXPIRED)
+        }
+    }
+
+    fun decodedJwt(token: String): DecodedJWT = JWT.require(Algorithm.HMAC256(secretKey)).build().verify(token)
 }
