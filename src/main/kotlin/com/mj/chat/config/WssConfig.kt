5448d4eff3a3e38e1298f4dd83788c33b3a8290f
mingj7235 <joshuara7235@gmail.com>
2024-12-07 22:41:16 +0900

    feat: websocket configuration basic setting

--- src/main/kotlin/com/mj/chat/config/WssConfig.kt
+++ src/main/kotlin/com/mj/chat/config/WssConfig.kt
@@ -1,22 +1,27 @@
 package com.mj.chat.config
 
-import com.mj.chat.component.WssHandlerV1
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.socket.WebSocketHandler
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocket
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
-import org.springframework.web.socket.handler.TextWebSocketHandler
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 @Configuration
 @EnableWebSocket
-class WssConfig(
-    private val wssHandlerV1: WssHandlerV1,
-) : WebSocketConfigurer {
-    override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
+class WssConfig : WebSocketMessageBrokerConfigurer {
+    // pub sub 에 대한 명시
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
         registry
-            .addHandler(wssHandlerV1, "/ws/v1/chat")
+            .enableSimpleBroker("/sub")
+        registry
+            .setApplicationDestinationPrefixes("/pub")
+    }
+
+    // web socket 통신에 연결되는 기본적인 연결 설정
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry
+            .addEndpoint("/ws-stomp")
             .setAllowedOrigins("*")
+//            .withSockJS() // 이 옵션은 client 가 websocket 을 사용할 수 없는 경우에, 방어로직으로 사용할 수 있는 옵션 (브라우저 버전이 너무 낮아서 websocket 을 지원하지 않는 경우) 이 옵션을 키면 http 통신을 함 (long polling)
     }
 }
